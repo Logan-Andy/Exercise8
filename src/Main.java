8bd0d5e6f3de45422a4498260458d729fc2b8f8b
Logan-Andy <148874243+Logan-Andy@users.noreply.github.com>
2025-03-05 13:02:52 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 //Logan Andy
-//Exercise 7
-// 2/25/2025
+//Exercise 8
+// 3/5/2025
 public class Main {
     public static void main(String[] args) {
         ContactInfo contactInfo1 = new ContactInfo("Logan", "awesomejandy@gmail.com");
         //contactInfo1.display();
         BusinessInfo businessInfo1 = new BusinessInfo("Omar", "116234@swcsd.us","614-580-0010");
         //businessInfo1.display();
-        Both both1 = new Both(contactInfo1, businessInfo1);
-        System.out.println(both1);
+        Both.addContact(contactInfo1);
+        Both.addContact(businessInfo1);
+        Both.displayContacts();
     }
 }
